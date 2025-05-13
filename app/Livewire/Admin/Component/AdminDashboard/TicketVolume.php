38f621060f45c9c9f5f41c355f73ab66fab03b40
Ronel028 <floridaronel15@gmail.com>
2025-05-13 09:51:08 +0800

    setup function for getting ticket volumn using date range and display from the line graph

--- app/Livewire/Admin/Component/AdminDashboard/TicketVolume.php
+++ app/Livewire/Admin/Component/AdminDashboard/TicketVolume.php
@@ -10,6 +10,8 @@ class TicketVolume extends Component
 {
 
     public $ticketCountPerDate = [];
+    public $startDate;
+    public $endDate;
 
     public function mount()
     {
@@ -31,26 +33,62 @@ class TicketVolume extends Component
         //     ];
         // }
 
-        $ticketCounts = \App\Models\Ticket::selectRaw("DATE_FORMAT(created_at, '%Y-%m') as month, COUNT(*) as count")
-            ->whereDate('created_at', '>=', now()->subMonths(5)->startOfMonth()) // includes current month + last 5
-            ->groupByRaw("DATE_FORMAT(created_at, '%Y-%m')")
-            ->orderByRaw("DATE_FORMAT(created_at, '%Y-%m')")
-            ->pluck('count', 'month');
 
-        $startMonth = now()->subMonths(5)->startOfMonth(); // 6 months ago
-        $endMonth = now()->startOfMonth(); // current month
+        // $ticketCounts = Ticket::selectRaw("DATE_FORMAT(created_at, '%Y-%m') as month, COUNT(*) as count")
+        //     ->whereDate('created_at', '>=', now()->subMonths(5)->startOfMonth()) // includes current month + last 5
+        //     ->groupByRaw("DATE_FORMAT(created_at, '%Y-%m')")
+        //     ->orderByRaw("DATE_FORMAT(created_at, '%Y-%m')")
+        //     ->pluck('count', 'month');
 
-        for ($month = $startMonth->copy(); $month->lte($endMonth); $month->addMonth()) {
-            $formatted = $month->format('Y-m');
+        // dd($ticketCounts);
+
+        // $startMonth = now()->subMonths(5)->startOfMonth(); // 6 months ago
+        // $endMonth = now()->startOfMonth();
+
+        // for ($month = $startMonth->copy(); $month->lte($endMonth); $month->addMonth()) {
+        //     $formatted = $month->format('Y-m');
+        //     $this->ticketCountPerDate[] = [
+        //         'date' => $formatted,
+        //         'count' => $ticketCounts->get($formatted, 0)
+        //     ];
+        // }
+
+        $this->startDate = now()->startOfMonth()->toDateString();
+        $this->endDate = now()->endOfMonth()->toDateString();
+        $this->getTicketVolumn($this->startDate, $this->endDate);
+    }
+
+    public function updated($property, $value)
+    {
+        if ($property == 'endDate') {
+            $this->endDate = $value;
+        }
+
+        $this->dispatch('refresh-chart');
+    }
+
+    public function getTicketVolumn($startDate, $endDate)
+    {
+        $stDate = Carbon::parse($startDate);
+        $edDate = Carbon::parse($endDate);
+        $tickets = Ticket::whereBetween('created_at', [$stDate, $edDate])->get();
+        for ($day = $stDate->copy(); $day->lte($edDate); $day->addDay()) {
+            $dateString = $day->format('Y-m-d');
+
+            $count = $tickets->filter(function ($ticket) use ($dateString) {
+                return $ticket->created_at->format('Y-m-d') === $dateString;
+            })->count();
+
             $this->ticketCountPerDate[] = [
-                'date' => $formatted,
-                'count' => $ticketCounts->get($formatted, 0)
+                'date' => $dateString,
+                'count' => $count,
             ];
         }
     }
 
     public function render()
     {
+        // $this->getTicketVolumn($this->startDate, $this->endDate);
         return view('livewire.admin.component.admin-dashboard.ticket-volume');
     }
 }
--- resources/views/livewire/admin/component/admin-dashboard/ticket-volume.blade.php
+++ resources/views/livewire/admin/component/admin-dashboard/ticket-volume.blade.php
@@ -1,6 +1,20 @@
 <div x-data="ticketVolume" class="mt-6">
     <div class=" bg-gray-100 border border-gray-300 shadow rounded p-3">
-        <canvas id="ticketVolume"></canvas>
+        <div>
+            <p class=" font-bold">Date Range</p>
+            <div class="flex items-center gap-2">
+                <div class=" flex-shrink">
+                    <x-forms.input-field type="date" wire:model.live.debounce.250ms="startDate" />
+                </div>
+                <p class="text-sm">To</p>
+                <div class=" flex-shrink">
+                    <x-forms.input-field type="date" wire:model.live.debounce.250ms="endDate" />
+                </div>
+            </div>
+        </div>
+        <div wire:ignore>
+            <canvas id="ticketVolume"></canvas>
+        </div>
     </div>
 
     @push('scripts')
@@ -8,10 +22,13 @@
             document.addEventListener('livewire:init', () => {
                 Alpine.data('ticketVolume', () => ({
                     ticketCountPerDate: @entangle('ticketCountPerDate'),
+                    startDate: @entangle('startDate'),
+                    endDate: @entangle('endDate'),
                     ticketDate: [],
                     ticketCount: [],
                     init(){
-                        console.log(this.ticketCountPerDate)
+                        let chartInstance;
+                        const lineChartId = document.getElementById('ticketVolume')
                         this.ticketCountPerDate.forEach(value => {
                             this.ticketDate.push(value.date)
                             this.ticketCount.push(value.count)
@@ -43,8 +60,15 @@
                             }
                         };
 
-                        new Chart(document.getElementById('ticketVolume'), config)
-                    }
+                        chartInstance = new Chart(lineChartId, config)
+
+                        Livewire.on('refresh-chart', () => {
+                            if(chartInstance){
+                                chartInstance.destroy()
+                            }
+                            chartInstance = new Chart(lineChartId, config)
+                        })
+                    },
                 }))
             })
         </script>
